3293f5fb25e1e705aa633725b64196bd1d44ba73
Sarhad Maisoon Salam <sarhadmaisoon18@gmail.com>
2017-08-21 00:48:38 -0400

    Hello OPENcv setup complete

---
+++ src/main/java/processing/HelloOpenCV.java
@@ -0,0 +1,36 @@
+package processing;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
+/**
+ * Class Details:-
+ * Author: Sarhad
+ * User: sarha
+ * Date: 10-Jul-17
+ * Time : 3:28 PM
+ * Project Name: CandyAI
+ * Class Name: HelloOpenCV
+ */
+public class HelloOpenCV
+{
+	
+	static
+	{
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to OpenCV " + Core.VERSION);
+		Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
+		System.out.println("OpenCV Mat: " + m);
+		Mat mr1 = m.row(1);
+		mr1.setTo(new Scalar(1));
+		Mat mc5 = m.col(5);
+		mc5.setTo(new Scalar(5));
+		System.out.println("OpenCV Mat data:\n" + m.dump());
+	}
+}
